9f5e9c4fa4d268b004e7da8cb989b1f23b3a0a0b
CristianSantacruz <santacruz-guarquila@hotmail.com>
2018-02-18 12:31:19 -0500

    Proyecto AppWeb

---
+++ ModeloDatosAppWebMuseo/DB_AppWebMuseo.sql
@@ -0,0 +1,49 @@
+/*==============================================================*/
+/* DBMS name:      Microsoft SQL Server 2005                    */
+/* Created on:     7/2/2018 7:46:13                             */
+/*==============================================================*/
+
+
+if exists (select 1
+            from  sysobjects
+           where  id = object_id('ADMINISTRADOR')
+            and   type = 'U')
+   drop table ADMINISTRADOR
+go
+
+if exists (select 1
+            from  sysobjects
+           where  id = object_id('RESERVA_ECOLOGICA')
+            and   type = 'U')
+   drop table RESERVA_ECOLOGICA
+go
+
+/*==============================================================*/
+/* Table: ADMINISTRADOR                                         */
+/*==============================================================*/
+create table ADMINISTRADOR (
+   IDADMINISTRADOR      int                  identity,
+   NOMBRESADMINISTRADOR varchar(25)          not null,
+   APELLIDOSADMINISTRADOR varchar(25)          not null,
+   LOGINADMINISTRADOR   varchar(15)          not null,
+   PASSWORDADMINISTRADOR varchar(10)          not null,
+   constraint PK_ADMINISTRADOR primary key nonclustered (IDADMINISTRADOR)
+)
+go
+
+/*==============================================================*/
+/* Table: RESERVA_ECOLOGICA                                     */
+/*==============================================================*/
+create table RESERVA_ECOLOGICA (
+   IDRESERVAECOLOGICA   int                  identity,
+   NOMBRERESERVAECOLOGICA varchar(50)          not null,
+   IMAGENRESERVAECOLOGICA image                null,
+   PROVINCIARESERVAECOLOGICA varchar(25)          not null,
+   DESCRIPCIONRESERVAECOLOGICA varchar(500)         not null,
+   LATITUDRESERVAECOLOGICA varchar(15)          not null,
+   LONGITUDRESERVAECOLOGICA varchar(15)          not null,
+   SITIOWEBRESERVAECOLOGICA varchar(100)         null,
+   constraint PK_RESERVA_ECOLOGICA primary key nonclustered (IDRESERVAECOLOGICA)
+)
+go
+
